5b07ed2e964e00a6ddc04aefcb379a5cf7a56691
Maxi <50016128+MaxiAmZocken@users.noreply.github.com>
2022-04-04 23:01:18 +0200

    added support for mobile devices

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,29 @@
 module.exports = {
+  mode: 'jit',
   darkMode: "class",
   content: [
-    "./public/*.html"
+    "./*.html"
   ],
   theme: {
     extend: {},
+    screens: {
+      'xs': '400px',
+      // => @media (min-width: 400px) { ... }
+      'sm': '640px',
+      // => @media (min-width: 640px) { ... }
+
+      'md': '768px',
+      // => @media (min-width: 768px) { ... }
+
+      'lg': '1024px',
+      // => @media (min-width: 1024px) { ... }
+
+      'xl': '1280px',
+      // => @media (min-width: 1280px) { ... }
+
+      '2xl': '1536px',
+      // => @media (min-width: 1536px) { ... }
+    }
   },
   plugins: [],
 }
