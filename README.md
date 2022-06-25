739ad3bace82c1e8e5fcd315b4416d3f820a32ca
Maxi <50016128+MaxiAmZocken@users.noreply.github.com>
2022-06-25 19:38:06 +0200

    optimised some stuff and added some from the ToDo

--- README.md
+++ README.md
@@ -2,9 +2,10 @@
 Welcome to the repository of my website, if you're interested how I made it you can view the files 
 # ToDo's
 
+- [x] mobile support
+- [x] add home button in dropdown
+- [x] fix instagram button
 - [ ] create containers as grids
 - [ ] add pictures in achievement.hmtl
 - [ ] fix youtube embeds
-- [x] mobile support
-- [ ] add home button in dropdown
-- [ ] fix instagram button
+
--- tailwind.config.js
+++ tailwind.config.js
@@ -5,25 +5,28 @@ module.exports = {
     "./*.html"
   ],
   theme: {
-    extend: {},
-    screens: {
-      'xs': '400px',
-      // => @media (min-width: 400px) { ... }
-      'sm': '640px',
-      // => @media (min-width: 640px) { ... }
-
-      'md': '768px',
-      // => @media (min-width: 768px) { ... }
-
-      'lg': '1024px',
-      // => @media (min-width: 1024px) { ... }
-
-      'xl': '1280px',
-      // => @media (min-width: 1280px) { ... }
-
-      '2xl': '1536px',
-      // => @media (min-width: 1536px) { ... }
-    }
+    extend: {
+      animation: {
+        cursor: 'cursor .6s linear infinite alternate',
+        type: 'type 1.8s ease-out .8s 1 normal both',
+        'type-reverse': 'type 1.8s ease-out 0s infinite alternate-reverse both',
+      },
+      keyframes: {
+        type: {
+          '0%': { width: '0ch' },
+          '5%, 10%': { width: '1ch' },
+          '15%, 20%': { width: '2ch' },
+          '25%, 30%': { width: '3ch' },
+          '35%, 40%': { width: '4ch' },
+          '45%, 50%': { width: '5ch' },
+          '55%, 60%': { width: '6ch' },
+          '65%, 70%': { width: '7ch' },
+          '75%, 80%': { width: '8ch' },
+          '85%, 90%': { width: '9ch' },
+          '95%': { width: '10ch' },
+        },
+      },
   },
-  plugins: [],
+  plugins: [require("daisyui")],
+}
 }
